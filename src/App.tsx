86791ddf7b5335bc63d30a22585b030b0700b76e
shutnikPk <shutnikssspk@gmail.com>
2021-08-31 16:10:50 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Table from "./components/Table";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Table />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,12 @@
-import React, { FC } from "react";
+import React, { useState, FC } from "react";
+import api from "../API";
+
+const users = api.users.fetchAll();
 
 export interface TableProps {}
 
 const Table: FC<TableProps> = () => {
+  const [users, setUsers] = useState([]);
   return <div></div>;
 };
 
